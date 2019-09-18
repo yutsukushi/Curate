811cc1b7c9ec8aa41a929af5f2a60e0f33494167
yutsukushi <ykushibi@gmail.com>
2019-09-17 23:05:36 -0400

    IT WORKS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import SignIn from "./components/Authentication/Signin"
+import SignIn from "./components/Authentication/Signin"
 import NavBar from "./components/NavBar";
-import Header from "./components/Header";
-import SearchForm from "./components/SearchForm";
+// import Header from "./components/Header";
+// import SearchForm from "./components/SearchForm";
+import CreateAcc from "./components/Authentication/CreateAcc";
 import './App.css';
 
 function App() {
@@ -12,8 +13,11 @@ function App() {
     <div>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={SearchForm} />
-        <Route exact path="/artist" component={Header} />
+        <SignIn />
+        <Route exact path="/login" component={ SignIn }/>
+        <Route exact path="/createaccount/" component={ CreateAcc }/>
+        {/* <Route exact path="/" component={SearchForm} />
+        <Route exact path="/artist" component={Header} /> */}
         {/* <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} /> */}
       </Switch>
--- src/components/Authentication/Signin.js
+++ src/components/Authentication/Signin.js
@@ -1,59 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+// import CreateAcc from "./CreateAcc";
 
-class SignIn extends Component {
-  // Setting the initial values of this.state.username and this.state.password
-  state = {
-    username: "",
-    password: ""
-  };
+function SignIn(props) {
 
-  // handle any changes to the input fields
-  handleInputChange = event => {
-    // Pull the name and value properties off of the event.target (the element which triggered the event)
-    const { name, value } = event.target;
-
-    // Set the state for the appropriate input field
-    this.setState({
-      [name]: value
-    });
-  };
-
-  // When the form is submitted, prevent the default event and alert the username and password
-  handleFormSubmit = event => {
-    event.preventDefault();
-    alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
-    this.setState({ username: "", password: "" });
-  };
-
-  render() {
-    return (
-      <form action="/login" method="POST">
-        <p>Username: {this.state.username}</p>
-        <p>Password: {this.state.password}</p>
+  return (
+    
+    <div>
+    <BrowserRouter>
+    <form action="/login" method="GET">
+        <p>Username: {props.username}</p>
+        <p>Password: {props.password}</p>
         <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          value={this.state.username}
-          onChange={this.handleInputChange}
+        type="text"
+        placeholder="Username"
+        name="username"
+        value={props.username}
+        onChange={props.handleInputChange}
         />
         <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleInputChange}
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={props.password}
+        onChange={props.handleInputChange}
         />
         <button
-        onClick={this.handleFormSubmit}>
+        onClick={props.handleLogInSubmit}>
             Sign In
         </button>
-        <button onClick={this.handleFormSubmit}>
-            Create Account
-        </button>
-      </form>
-    );
-  }
+        
+        <div>
+            <Link to="/createaccount">
+              Create Account
+            </Link>
+        </div>
+    </form>
+    </BrowserRouter>
+    </div>
+);
+
 }
 
 export default SignIn;
