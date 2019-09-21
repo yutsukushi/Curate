36b34c10e843852a5394964639c0adb7f817e60c
Katie Vogel <kvog983@gmail.com>
2019-09-21 15:53:37 -0400

    book->artist api endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const artistRoutes = require("./artist");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/artists", artistRoutes);
 // setting up api route /api/books to look into the js file of books
 
 module.exports = router;
