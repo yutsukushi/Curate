bcc04b9abb5891af15d388b6832a2ba3c714467c
DaWood32913 <woodleyjohnson@yahoo.com>
2019-09-23 21:11:06 -0400

    heroku deploy

---
+++ client/build/precache-manifest.0a03c8a46c5bec73850b44b403b524ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93e94541036a49351091",
+    "url": "/static/js/main.a4007ecc.chunk.js"
+  },
+  {
+    "revision": "b4dce006e830b3d11f3d",
+    "url": "/static/js/2.c3f03221.chunk.js"
+  },
+  {
+    "revision": "93e94541036a49351091",
+    "url": "/static/css/main.dc2bee97.chunk.css"
+  },
+  {
+    "revision": "699c42d79933514a37a1e3a20b47ed55",
+    "url": "/index.html"
+  }
+];
