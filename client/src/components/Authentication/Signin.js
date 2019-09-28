fdcba506be1819358be3262580c2c75d0c5c011c
AJW <ajweber102@gmail.com>
2019-09-28 14:11:02 -0400

    Work dir styling for Signin page

--- client/src/components/Authentication/Signin.js
+++ client/src/components/Authentication/Signin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Link } from "react-router-dom";
+import "./style.css";
 // import CreateAcc from "./CreateAcc";
 
 function SignIn(props) {
@@ -8,34 +9,43 @@ function SignIn(props) {
     
     <div>
     <BrowserRouter>
-    <form action="/login" method="GET">
-        <span>Username: {props.username}</span>
-        <input
-        type="text"
-        placeholder="Username"
-        name="username"
-        value={props.username}
-        onChange={props.handleInputChange}
-        />
-        <span>Password: {props.password}</span>
-        <input
-        type="password"
-        placeholder="Password"
-        name="password"
-        value={props.password}
-        onChange={props.handleInputChange}
-        />
-        <button
-        onClick={props.handleLogInSubmit}>
-            Sign In
-        </button>
-        
-        <div>
-            <Link to="/createaccount">
-              Create Account
-            </Link>
-        </div>
-    </form>
+    <div className="containerLogin">
+      <div className="loginForm">
+        <form action="/login" method="GET">
+          <div>
+            <span>Username: {props.username}</span>
+            <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={props.username}
+            onChange={props.handleInputChange}
+            />
+          </div>
+          <div>
+            <span>Password: {props.password}</span>
+            <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={props.password}
+            onChange={props.handleInputChange}
+            />
+            <div>
+              <button
+              onClick={props.handleLogInSubmit}>
+                  Sign In
+              </button>
+            </div>
+          </div>
+            <div>
+                <Link to="/createaccount">
+                  Create Account
+                </Link>
+            </div>
+        </form>
+      </div>
+    </div>
     </BrowserRouter>
     </div>
 );
