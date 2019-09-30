82e025ce70ae173af8f0d0b0d49e893c204ff494
yutsukushi <ykushibi@gmail.com>
2019-09-30 19:27:45 -0400

    redirecting nav links

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,10 +6,10 @@ export function NavBar() {
     return (
         <div className="nav-link-container">
             <li className="nav-item nav-item-link">
-                <Link to="/login" className="nav-link">Login</Link>
+                <Link to="/" className="nav-link">Logout</Link>
             </li>
             <li className="nav-item nav-item-link">
-                <Link to="/" className="nav-link">Search</Link>
+                <Link to="/search" className="nav-link">Search</Link>
             </li>
             <li className="nav-item nav-item-link">
                 <Link to="/saved" className="nav-link">Saved</Link>
