6772febf37fcf01df5bf14843b21e5ebb5970bca
AJW <ajweber102@gmail.com>
2019-09-30 19:25:00 -0400

    restructured nav links

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,13 +6,13 @@ export function NavBar() {
     return (
         <div className="nav-link-container">
             <li className="nav-item nav-item-link">
-                <Link to="/login" className="nav-link">Login</Link>
-            </li>
-            <li className="nav-item nav-item-link">
                 <Link to="/" className="nav-link">Search</Link>
             </li>
             <li className="nav-item nav-item-link">
                 <Link to="/saved" className="nav-link">Saved</Link>
+            </li>
+            <li className="nav-item nav-item-link">
+                <Link to="/login" className="nav-link">Log out</Link>
             </li>
         </div>
     )
