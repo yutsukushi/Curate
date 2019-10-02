ee9bea998778fe2a8915c28a40190e4c3c2f59db
AJW <ajweber102@gmail.com>
2019-10-02 15:25:51 -0400

    modified card styling

--- client/src/components/ImageCard/index.js
+++ client/src/components/ImageCard/index.js
@@ -5,21 +5,23 @@ import "./Card.css";
 //stateless component
 const ImageCard = props => {
   return (
-    <div className="card img-container hover">
-    <img src={props.thumbnail} alt="artwork"></img>
-        <ul>
-          <li className="art_title">{props.title}</li>
-          <li className="artist_name">{props.name}</li>
-          <li className="medium">{props.medium}</li>
-          <li className="art_date">{props.date}</li>
-          <li className="nationality">{props.nationality}</li>
-        </ul>
-        <form action="/saved" method="POST">
-        <button className="saveArt" 
-        data-id={props.cardId}
-        onClick={props.handleSaveArtist}>
-        +</button></form>
-        <button className="profileButton" >View Profile</button>
+    <div className="card">
+      <img src={props.thumbnail} alt="artwork" className="hero-image"></img>
+      <div>
+          <ul className="card-list">
+            <li className="art_title">{props.title}</li>
+            <li className="artist_name">{props.name}</li>
+            <li className="medium">{props.medium}</li>
+            <li className="art_date">{props.date}</li>
+            <li className="nationality">{props.nationality}</li>
+          </ul>
+          <form action="/saved" method="POST">
+          <button className="saveArt" 
+          data-id={props.cardId}
+          onClick={props.handleSaveArtist}>
+          +</button></form>
+          <button className="profileButton">View Profile</button>
+      </div>
     </div>
   );
 }
