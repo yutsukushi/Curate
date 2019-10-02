5c3e891e13d4ddeb2022816d5c2614c1e4a1df97
Katie Vogel <kvog983@gmail.com>
2019-10-01 22:32:15 -0400

    renamed to PopulateArtist for clarity; componentDidMount does page load

--- client/src/components/SavedArtists/Saved.js
+++ client/src/components/SavedArtists/Saved.js
@@ -10,73 +10,94 @@ import _ from "lodash";
 
 class Saved extends Component {
 
-    constructor (props) {
+    constructor(props) {
         super(props)
         this.state = {
-          savedArt: []
+            savedArt: ""
         }
-        this.handleDeleteArtist.bind(this);
-        this.handleSavedArtist.bind(this);
     }
+    handlePopulateArtist = () => {
+        Axios.get("/saved", {
+            params: {
+                username: this.state.username
+            }
+        })
+            .then(res => {
+                console.log("saved artists: ", res);
+                this.setState({ savedArt: res.data.favorite_artworks });
+            })
+    }
+    componentDidMount() {
+        this.handlePopulateArtist();
+    };
+    // this.handlePopulateArtist.bind(this);
 
     // TODO:
     // when saved tab is clicked,
     // get the saved artworks and load page
     // render saved art cards with delete button
     // delete button should appear for each card, and delete functionality
-    
-    handleSavedArtist = () => {
-        Axios.get("/saved", {
-            params: {
-                savedArt: this.state.savedArt
-            }
-        })
-        .then(res => {
-            console.log("saved artists: ", res)
-        })
-    }
+    // this.handleDeleteArtist.bind(this);
 
     handleDeleteArtist = (event) => {
         event.preventDefault();
 
-        let removeArtwork = _.find(this.state.serverResponse, { '_id': event.target.dataset.id});
+        let removeArtwork = _.find(this.state.serverResponse, { '_id': event.target.dataset.id });
         console.log("remove artwork" + removeArtwork)
         Axios.delete("/saved", removeArtwork);
     }
 
     render() {
         return (
-            <div className="header">
-                <Logo />
-                <NavBar handleSavedArtist={this.handleSavedArtist}/>
-                <h4>Saved</h4>
-                <div className="savedArt">
-                    {this.state.savedArt.length ? (
-                        <List>
-                        {this.state.savedArt.map(art => (
-                            <ListItem key={art._id}>
-                                <ImageCard 
-                                thumbnail={art.ThumbnailURL} 
-                                artId={art._id}
-                                title={art.Title} 
-                                date={art.Date} 
-                                medium={art.Medium}
-                                name={art.Artist} 
-                                nationality={art.Nationality}
-                                handleDeleteArtist={this.handleDeleteArtist} 
-                                /> 
-                                <DeleteBtn />
-                            </ListItem>
-                        ))}
-                        </List>
-                    ) : (
-                        <h5>Saved works here.</h5>
-                    )}
+            <div className="box">
+                <div className="header">
+                    <Logo />
+                    <NavBar/>
+                    </div>
+                    <div className="body">
+                    {/* handlePopulateArtist={this.handlePopulateArtist}  */}
+                    
+                    <h4>Saved</h4>
+                    <div className="savedArt">
+                        {this.state.savedArt.length ? (
+                            <List>
+                                {this.state.savedArt.map(art => {
+                                    debugger;
+                                    return (
+                                        <ListItem 
+                                        key={art._id}
+                                        alt={art.Artist} 
+                                        image={art.ThumbnailURL}>
+                                            <ImageCard
+                                                thumbnail={art.ThumbnailURL}
+                                                artId={art._id}
+                                                title={art.Title}
+                                                date={art.Date}
+                                                medium={art.Medium}
+                                                name={art.Artist}
+                                                nationality={art.Nationality}
+                                                handleDeleteArtist={this.handleDeleteArtist}
+                                            />
+                                            <DeleteBtn />
+                                        </ListItem>
+                                    );
+                                })}
+                            </List>
+                        ) : ( 
+                                <h5>Saved works here.</h5>
+                            )}
+                    </div>
+
                 </div>
+                {/* TESTING PURPOSEs */}
+                < pre > {JSON.stringify(this.state, null, 2)}</pre >
             </div>
+
+
+
         )
     }
-    
+
 }
 
 export default Saved;
