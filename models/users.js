9c15b039821120356f13d32d442ee7a90f92811e
DaWood32913 <woodleyjohnson@yahoo.com>
2019-10-03 19:39:11 -0400

    password enctyption

--- models/users.js
+++ models/users.js
@@ -37,9 +37,25 @@ var UserSchema = new Schema({
 
 var User = mongoose.model("User", UserSchema);
 
-// module.exports = mongoose.model('User',{
-//     username: String,
-//     password: String
-// });
+var bcrypt = require('bcrypt');
+
+exports.cryptPassword = function(password, callback) {
+   bcrypt.genSalt(10, function(err, salt) {
+    if (err) 
+      return callback(err);
+
+    bcrypt.hash(password, salt, function(err, hash) {
+      return callback(err, hash);
+    });
+  });
+};
+
+exports.comparePassword = function(plainPass, hashword, callback) {
+   bcrypt.compare(plainPass, hashword, function(err, isPasswordMatch) {   
+       return err == null ?
+           callback(null, isPasswordMatch) :
+           callback(err);
+   });
+};
 
 module.exports = User;
