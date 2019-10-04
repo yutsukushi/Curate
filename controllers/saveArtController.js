b7bb3c33dc8db1db903970cc454ced58d7b5a3fe
Katie Vogel <kvog983@gmail.com>
2019-10-03 21:18:43 -0400

    remove art info

--- controllers/saveArtController.js
+++ controllers/saveArtController.js
@@ -6,14 +6,14 @@ const ARTIST_FIELDS = 'Title Artist Nationality Date URL Medium ThumbnailURL';
 
 module.exports = {
   findUserAndPW: function (req, res) {
-    console.log('findAll req.query: ', req.query);
+    // console.log('findAll req.query: ', req.query);
     var userName = req.query.username; // e.g. "usernameXYZ1"
     var passWord = req.query.password; // e.g. "password123"
 
     db.User
       .findOne({ username: userName }, USER_FIELDS)
       .then(userRecord => {
-        console.log(userRecord);
+        // console.log(userRecord);
         if (userRecord !== null && userRecord.password === passWord) {
           res.cookie('username', userRecord.username)
           res.json(userRecord);
@@ -26,17 +26,17 @@ module.exports = {
   },
 
   findAndSaveArt: async function (req, res) {
-    console.log(`findAndSaveArt 
-    req.query: ${(JSON.stringify(req.query))} 
-    cookies: ${JSON.stringify(req.cookies)} 
-    req.body: ${JSON.stringify(req.body)}`);
+    // console.log(`findAndSaveArt 
+    // req.query: ${(JSON.stringify(req.query))} 
+    // cookies: ${JSON.stringify(req.cookies)} 
+    // req.body: ${JSON.stringify(req.body)}`);
     var userLogin = req.cookies.username;
     var favArtworkId = req.body._id;
 
     try {
       let userArts = await db.User.findOne({ username: userLogin });
-      console.log("userArts: " + userArts);
-      console.log("userArts: " + userArts.favorite_artworks)
+      // console.log("userArts: " + userArts);
+      // console.log("userArts: " + userArts.favorite_artworks)
       let artID = _.find(userArts.favorite_artworks, { _id: favArtworkId });
       console.log("artID: " + artID)
       if (artID === undefined) {
@@ -44,7 +44,7 @@ module.exports = {
         let userArts2 = await db.User.findOneAndUpdate(
           { username: userLogin },
           { $push: { favorite_artworks: req.body } });
-        console.log("userArts2: " + userArts2)
+        // console.log("userArts2: " + userArts2)
         return res.json(userArts2);
       }
       else {
@@ -61,46 +61,34 @@ module.exports = {
 
   findAndDeleteArt: async function (req, res) {
     var userLogin = req.cookies.username;
-    var favArtworkId = req.body._id;
-
     try {
-      let userSavedArts = await db.User.findOne({ username: userLogin });
-      console.log("userSavedArts: " + userSavedArts.favorite_artworks)
-      let savedArtID = _.find(userSavedArts.favorite_artworks, { _id: favArtworkId });
-      console.log("savedArtID: " + savedArtID)
-      if (savedArtID !== undefined) {
-        // delete new fav art into user record
-        let userSavedArts2 = await db.User.findOneAndUpdate(
-          { username: userLogin },
-          { $pull: { favorite_artworks: req.body } });
-        console.log("userSavedArts2: " + userSavedArts2)
-        return res.json(userSavedArts2);
-      }
-      else {
-        // this artwork is already favorited by this user
-        return res.status(420).json({ "error": "It is not possible to delete that which does not exist" });
-      }
+      let result = await db.User.findOneAndUpdate({ username: userLogin }, { $pull: { favorite_artworks: { _id: req.params.id } } });
+      return res.json({});
+    }
+    catch (e) {
+      console.log(e)
+      return res.status(422).json(e);
+    }
 
-    }
-    catch (err) {
-      console.log(err)
-      return res.status(422).json(err);
-    }
+  },
+  catch(err) {
+    console.log(err)
+    return res.status(422).json(err);
+    // }
 
   },
 
   findAndPopulate: function (req, res) {
-    console.log('findAndPopulate req.query: ', req.query);
-    console.log('findAndPopulate req.cookies: ', req.cookies);
+    // console.log('findAndPopulate req.query: ', req.query);
+    // console.log('findAndPopulate req.cookies: ', req.cookies);
     var userLogin = req.cookies.username;
 
     db.User
       .findOne({ username: userLogin }, 'favorite_artworks')
       .then(displayArt => {
-        console.log("displayArt: " + displayArt);
+        // console.log("displayArt: " + displayArt);
         return res.json(displayArt);
       })
       .catch(err => res.status(422).json(err));
   }
 };
- 
