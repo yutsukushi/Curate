e08580aac09284aecf0006dcca3bb5cb9beba05b
yutsukushi <ykushibi@gmail.com>
2019-10-31 15:54:49 -0400

    working modal set up when images are clicked

--- client/src/components/ImageCard/index.js
+++ client/src/components/ImageCard/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Modal from "../Modal"
 import "./Card.css";
 
 function ImageCard ({thumbnail, title, name, medium, url, date, nationality, Button, handleFullSizeImage}) {
+  
   return (
     <div className="card">
       <img src={thumbnail} alt="artwork" className="card-img" 
-       ></img>
+      onClick={(event) => handleFullSizeImage(url, event)}/>
+      
       <div className="card-list">
         <ul className="card-info">
           <li className="art_title">{title}</li>
@@ -16,6 +19,7 @@ function ImageCard ({thumbnail, title, name, medium, url, date, nationality, But
           <li className="url"><a href={url} target="_blank">More Info</a></li>
         </ul>
         <Button />
+        <Modal />
       </div>
     </div>
   );
